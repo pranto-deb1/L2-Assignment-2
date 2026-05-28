484eac01952953e4de3d0522e704f3299da1f98b
pranto-deb1 <prizmine123@gmail.com>
2026-05-28 21:21:22 +0600

    deploying to vercel

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authController } from "./auth.controller";
 
 const route = Router();
-route.post("/register", authController.registerUser);
+route.post("/signup", authController.registerUser);
 route.post("/login", authController.loginUser);
 
 export const authRoute = route;
